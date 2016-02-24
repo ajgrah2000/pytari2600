2f727d6c1cef331cab272b4e107819a3212873b8
Andrew <andrew@localhost>
2016-02-25 06:59:42 +1100

    Updated unit_test module path issues between python 2 and 3

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,3 @@
-import unittest
 from pytari2600.test.test_memory       import *
 from pytari2600.test.test_stella       import *
 from pytari2600.test.test_cartridge    import *
@@ -7,6 +6,7 @@ from pytari2600.test.test_addressing   import *
 from pytari2600.test.test_input        import *
 from pytari2600.test.test_instructions import *
 from pytari2600.test.test_tiasound     import *
+import unittest
 
 if __name__ == '__main__':
     unittest.main()
