95daaab79f00fa540e16d940862629fae740dffd
Andrew <andrew@localhost>
2016-02-25 00:54:18 +1100

    Updating unit_tests for python 3

--- pytari2600/unit_tests.py
+++ pytari2600/unit_tests.py
@@ -1,12 +1,12 @@
 import unittest
-from test.test_memory       import *
-from test.test_stella       import *
-from test.test_cartridge    import *
-from test.test_riot         import *
-from test.test_addressing   import *
-from test.test_input        import *
-from test.test_instructions import *
-from test.test_tiasound     import *
+from pytari2600.test.test_memory       import *
+from pytari2600.test.test_stella       import *
+from pytari2600.test.test_cartridge    import *
+from pytari2600.test.test_riot         import *
+from pytari2600.test.test_addressing   import *
+from pytari2600.test.test_input        import *
+from pytari2600.test.test_instructions import *
+from pytari2600.test.test_tiasound     import *
 
 if __name__ == '__main__':
     unittest.main()
