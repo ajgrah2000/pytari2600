5b443d0f29abf20203f3d7a76c4a32d62ab74aaf
Andrew <a@hotmail.com>
2018-08-17 09:36:14 -0400

    Improving sound by stretching to match callback size.

--- pytari2600/audio/sounddeviceaudio.py
+++ pytari2600/audio/sounddeviceaudio.py
@@ -5,6 +5,8 @@ class SoundDeviceSound(tiasound.TIA_Sound):
     """ Sound, output to the 'sounddevice' module.
     """
 
+    BLOCKSIZE=1024
+
     def __init__(self, clocks):
         super(SoundDeviceSound, self).__init__(clocks)
 
@@ -13,17 +15,17 @@ class SoundDeviceSound(tiasound.TIA_Sound):
         self._next_buffer_0 = bytearray([0,0])
         self._next_buffer_1 = bytearray([0,0])
 
+        self._next_buffer_stretch_0 = [0] * self.BLOCKSIZE
+        self._next_buffer_stretch_1 = [0] * self.BLOCKSIZE
+
         self._last_update_time = self.clocks.system_clock
-
-        # Hold 'stretch' state for each channel.
-        self._stretcher = tiasound.Stretch()
-
-        self._stretcher.rate = 1000 # 1000 is no stretch.
 
         self.openSound()
 
+        print self.stretch([1,2,3,5], 7)
+
     def openSound(self):
-        self.stream = sounddevice.Stream(channels=2, blocksize=0, samplerate=self.SAMPLERATE,callback=self.fill_me, finished_callback=self.finished_callback)
+        self.stream = sounddevice.Stream(channels=2, blocksize=self.BLOCKSIZE, samplerate=self.SAMPLERATE,callback=self.fill_me, finished_callback=self.finished_callback)
 
         self.stream.start()
         
@@ -37,8 +39,12 @@ class SoundDeviceSound(tiasound.TIA_Sound):
         self._raw_audio[1].extend(self.get_channel_data(1, (self.SAMPLERATE*audio_ticks/self.CPU_CLOCK_RATE)))
         self._last_update_time = self.clocks.system_clock
 
-        self._next_buffer_0 += self._stretcher.stretch(self._raw_audio[0])
-        self._next_buffer_1 += self._stretcher.stretch(self._raw_audio[1])
+        self._next_buffer_0 += bytearray(self._raw_audio[0])
+        self._next_buffer_1 += bytearray(self._raw_audio[1])
+
+        # 'Stretch' buffer to match callback block size.
+        self._next_buffer_stretch_0 = self.stretch(self._next_buffer_0, self.BLOCKSIZE)
+        self._next_buffer_stretch_1 = self.stretch(self._next_buffer_1, self.BLOCKSIZE)
 
         self._raw_audio[0] = []
         self._raw_audio[1] = []
@@ -46,13 +52,33 @@ class SoundDeviceSound(tiasound.TIA_Sound):
     def step(self):
         pass
 
+    def stretch(self, unstretched_source, final_length):
+        # Stretch source to end up with length 'final_length'
+        source_pos = 0
+        source_pos_remainder = 0
+
+        stretched_result = []
+
+        remainder = len(unstretched_source) % final_length
+        diviser = int(len(unstretched_source)/final_length)
+
+        while len(stretched_result) < final_length:
+            stretched_result.append(unstretched_source[source_pos])
+
+            source_pos_remainder += remainder
+            source_pos += diviser 
+            if source_pos_remainder > final_length:
+                source_pos += 1
+                source_pos_remainder -= final_length
+
+        return stretched_result
+
     def fill_me(self, indata, outdata, frames, time, status):
-        if len(self._next_buffer_0) < frames:
-            self._next_buffer_0 += bytearray([0] * (frames - len(self._next_buffer_0)))
-        if len(self._next_buffer_1) < frames:
-            self._next_buffer_1 += bytearray([0] * (frames - len(self._next_buffer_1)))
-        outdata[:,0] = self._next_buffer_0[:frames]
-        outdata[:,1] = self._next_buffer_1[:frames]
+        if status:
+            print status
+            print status.output_underflow
+        outdata[:,0] = self._next_buffer_stretch_0
+        outdata[:,1] = self._next_buffer_stretch_1
         self._next_buffer_0 = [0]
         self._next_buffer_1 = [0]
 
